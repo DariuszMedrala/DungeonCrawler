23ef9f0b19f04ac10533080b90fa8e804b74f1b1
Michal Kowal <michal.kowal.66@gmail.com>
2025-06-22 17:32:50 +0200

    Rename dragon sword

--- src/main/java/org/example/dungeonCrawler/model/items/weapons/DragonSword.java
+++ src/main/java/org/example/dungeonCrawler/model/items/weapons/DragonSword.java
@@ -2,6 +2,6 @@ package org.example.dungeonCrawler.model.items.weapons;
 
 public class DragonSword extends Weapon {
     public DragonSword() {
-        super("Smocza Klunga", "Legendarna broń wykuta z kości smoka. +25 do obrażeń", 25, WeaponType.SWORD);
+        super("Smocza Klinga", "Legendarna broń wykuta z kości smoka. +25 do obrażeń", 25, WeaponType.SWORD);
     }
 }
